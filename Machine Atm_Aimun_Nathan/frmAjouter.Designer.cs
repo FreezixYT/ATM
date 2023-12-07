c8c4b942ea1aa7a58d52b85f8c5d6077e8e53911
nathan.hacker.pch <nathan.hacker.pch@gmail.com>
2023-12-07 15:02:56 +0100

    disine ajouter

--- Machine Atm_Aimun_Nathan/frmAjouter.Designer.cs
+++ Machine Atm_Aimun_Nathan/frmAjouter.Designer.cs
@@ -28,45 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label lblAjouter;
             this.lblMontant = new System.Windows.Forms.Label();
             this.tbxMontant = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblArgentAjouter = new System.Windows.Forms.Label();
             this.tbxAjouter = new System.Windows.Forms.TextBox();
             this.btnEntrer = new System.Windows.Forms.Button();
             this.btnRetourner = new System.Windows.Forms.Button();
+            lblAjouter = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblMontant
             // 
             this.lblMontant.AutoSize = true;
-            this.lblMontant.Location = new System.Drawing.Point(79, 33);
+            this.lblMontant.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMontant.Location = new System.Drawing.Point(127, 81);
             this.lblMontant.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMontant.Name = "lblMontant";
-            this.lblMontant.Size = new System.Drawing.Size(77, 13);
+            this.lblMontant.Size = new System.Drawing.Size(116, 20);
             this.lblMontant.TabIndex = 0;
-            this.lblMontant.Text = "entrez balance";
+            this.lblMontant.Text = "Entrez balance";
             // 
             // tbxMontant
             // 
-            this.tbxMontant.Location = new System.Drawing.Point(197, 33);
+            this.tbxMontant.Location = new System.Drawing.Point(269, 83);
             this.tbxMontant.Margin = new System.Windows.Forms.Padding(2);
             this.tbxMontant.Name = "tbxMontant";
             this.tbxMontant.Size = new System.Drawing.Size(68, 20);
             this.tbxMontant.TabIndex = 1;
             // 
-            // label1
+            // lblArgentAjouter
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(17, 100);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(189, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "entrez l\'argent que vous voulez ajouter";
+            this.lblArgentAjouter.AutoSize = true;
+            this.lblArgentAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArgentAjouter.Location = new System.Drawing.Point(127, 117);
+            this.lblArgentAjouter.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblArgentAjouter.Name = "lblArgentAjouter";
+            this.lblArgentAjouter.Size = new System.Drawing.Size(123, 20);
+            this.lblArgentAjouter.TabIndex = 2;
+            this.lblArgentAjouter.Text = "Argent à ajouter";
             // 
             // tbxAjouter
             // 
-            this.tbxAjouter.Location = new System.Drawing.Point(231, 100);
+            this.tbxAjouter.Location = new System.Drawing.Point(269, 117);
             this.tbxAjouter.Margin = new System.Windows.Forms.Padding(2);
             this.tbxAjouter.Name = "tbxAjouter";
             this.tbxAjouter.Size = new System.Drawing.Size(68, 20);
@@ -74,33 +78,48 @@
             // 
             // btnEntrer
             // 
-            this.btnEntrer.Location = new System.Drawing.Point(185, 164);
+            this.btnEntrer.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnEntrer.Location = new System.Drawing.Point(131, 179);
             this.btnEntrer.Margin = new System.Windows.Forms.Padding(2);
             this.btnEntrer.Name = "btnEntrer";
             this.btnEntrer.Size = new System.Drawing.Size(79, 22);
             this.btnEntrer.TabIndex = 4;
             this.btnEntrer.Text = "btnEnter";
-            this.btnEntrer.UseVisualStyleBackColor = true;
+            this.btnEntrer.UseVisualStyleBackColor = false;
             // 
             // btnRetourner
             // 
-            this.btnRetourner.Location = new System.Drawing.Point(427, 263);
+            this.btnRetourner.BackColor = System.Drawing.Color.RosyBrown;
+            this.btnRetourner.Location = new System.Drawing.Point(443, 259);
             this.btnRetourner.Margin = new System.Windows.Forms.Padding(2);
             this.btnRetourner.Name = "btnRetourner";
             this.btnRetourner.Size = new System.Drawing.Size(79, 22);
             this.btnRetourner.TabIndex = 5;
-            this.btnRetourner.Text = "retourner";
-            this.btnRetourner.UseVisualStyleBackColor = true;
+            this.btnRetourner.Text = "Exit";
+            this.btnRetourner.UseVisualStyleBackColor = false;
+            // 
+            // lblAjouter
+            // 
+            lblAjouter.AutoSize = true;
+            lblAjouter.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblAjouter.Location = new System.Drawing.Point(168, 5);
+            lblAjouter.Name = "lblAjouter";
+            lblAjouter.Size = new System.Drawing.Size(191, 50);
+            lblAjouter.TabIndex = 6;
+            lblAjouter.Text = "AJOUTER";
+            lblAjouter.UseCompatibleTextRendering = true;
             // 
             // frmAjouter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
             this.ClientSize = new System.Drawing.Size(533, 292);
+            this.Controls.Add(lblAjouter);
             this.Controls.Add(this.btnRetourner);
             this.Controls.Add(this.btnEntrer);
             this.Controls.Add(this.tbxAjouter);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblArgentAjouter);
             this.Controls.Add(this.tbxMontant);
             this.Controls.Add(this.lblMontant);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -116,7 +135,7 @@
 
         private System.Windows.Forms.Label lblMontant;
         private System.Windows.Forms.TextBox tbxMontant;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblArgentAjouter;
         private System.Windows.Forms.TextBox tbxAjouter;
         private System.Windows.Forms.Button btnEntrer;
         private System.Windows.Forms.Button btnRetourner;
