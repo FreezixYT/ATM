9097c4a7fc15b828b1247d1e3b9751367e575d10
aimunymn <tewolde.ymn@eduge.ch>
2024-01-07 20:49:20 +0100

    2 commit maison

--- Machine Atm_Aimun_Nathan/frmMain.cs
+++ Machine Atm_Aimun_Nathan/frmMain.cs
@@ -81,7 +81,8 @@ namespace Machine_Atm_Aimun_Nathan
 
         private void btnOui_Click(object sender, EventArgs e)
         {
-            
+            AfficheCompte frm= new AfficheCompte();
+            frm.Show();
         }
 
         private void btnNon_Click(object sender, EventArgs e)
